a9614b5e09b65da0c3f8757e9dde56850e31968c
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-19 19:59:47 +0800

    first commit

---
+++ lists.py
@@ -0,0 +1,14 @@
+programming_languages = ["Python","JavaScript","C++","Java"]
+print(programming_languages[0])
+print(programming_languages[1])
+print(programming_languages[2])
+
+
+print(programming_languages[1:3])
+
+# Nested lists
+
+nested_lists=[["a","b","c"],["d","e"]]
+
+print(nested_lists[0])
+print(nested_lists[1])
