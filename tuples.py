a551e4f1a9f51641f01baed42b9e7db1f27eb243
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-19 23:39:31 +0800

    Adding tuples.py to my github Python_Loop repo.

---
+++ tuples.py
@@ -0,0 +1,10 @@
+my_tuple = ("a","b","c","d")
+
+print(my_tuple[0])
+print(my_tuple[1])
+print(my_tuple[2])
+print(my_tuple[3])
+
+#tuple methods
+#tuple_index = my_tuple.index("b")
+print(my_tuple.index("b"))
