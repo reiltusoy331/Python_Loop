9f40f51d7e705eb4e6e092451b6788bfeec9c5c6
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-20 19:28:39 +0800

    Adding dictionaryMethods.py to my github Python_Loop repo.

---
+++ dictionaryMethods.py
@@ -0,0 +1,15 @@
+grades = {"Anna":97, "Jane":90}
+
+#Get each keys 
+print(grades.keys())
+
+#Get each values 
+print(grades.values())
+
+#Get each items 
+print(grades.items())
+
+#Update key-value pairs
+grades.update({"Jane":92,"Dexter":85})
+print(grades)
+
