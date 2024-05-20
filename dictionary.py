030b248af748e62a44070fae43c5d48d5276be36
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-20 19:27:23 +0800

    Adding dictionary.py to my github Python_Loop repo.

---
+++ dictionary.py
@@ -0,0 +1,25 @@
+my_dict = {"A":45, "B":23, "C":24}
+
+#Get Value
+print(my_dict["A"])
+print(my_dict["B"])
+
+#Add Value
+my_dict["D"] = 32
+print(my_dict)
+
+#Modify a Value of a Key
+my_dict["A"] = 34
+print(my_dict)
+
+#Delete a key-value Pair
+
+del my_dict["A"]
+print(my_dict)
+
+
+#Get the number of key-value Pair of the dictionary
+print(len(my_dict))
+
+
+
