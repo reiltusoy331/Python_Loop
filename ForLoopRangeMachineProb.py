c649dc3d196910d14ad0384f4c0c8b4bb853710e
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-27 18:59:42 +0800

    Adding ForLoopRangeMachineProb.py to my github Python_Loop repo.

---
+++ ForLoopRangeMachineProb.py
@@ -0,0 +1,9 @@
+#### Machine Problem ####
+# Print all the substrings of length 2 excluding the first and last characters
+
+word = "programming"
+
+
+for i in range(1,len(word)-2):
+    print(word[i]+word[i+1]) # print(word[i:i+2])
+
