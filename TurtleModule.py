7d7b4b168afbda32addb4453a0a53bf2d6986caf
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-28 20:08:50 +0800

    Adding TurtleModule.py to my github Python_Loop repo.

---
+++ TurtleModule.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.circle(100)
+turtle.color("blue")
+turtle.width(3)
+
+
+turtle.penup()
+turtle.goto(150,-150)
+turtle.pendown()
+
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+
+
+turtle.exitonclick()
