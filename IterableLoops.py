208cf973b654917e1290a1feb062318da83a14ab
Reil Tusoy <reiltusoy331@gmail.com>
2024-05-28 20:09:30 +0800

    Adding IterableLoops.py to my github Python_Loop repo.

---
+++ IterableLoops.py
@@ -0,0 +1,25 @@
+ 
+print("Iterate in Strings")
+for char in "loops":
+    print(char, end=' ')
+
+print("\n")
+
+print("Iterate in Tuples")
+for num in (1,2,3,4):
+    print(num, end=' ')
+
+print("\n")
+
+print("Iterate in Sets") #Sets are unordered sequences
+for num in {1,2,3,4}:
+    print(num, end=' ')
+
+
+print("\n")
+
+print("Iterate in Dictionary") 
+for key in {"a":1, "b":2, "c":3}:
+    print(key, end=' ')
+
+ 
