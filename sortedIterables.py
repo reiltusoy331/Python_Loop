64393b95580fdb3fc2f35d6ee76af0f43a96c06f
Reil Tusoy <reiltusoy331@gmail.com>
2024-06-01 23:57:00 +0800

    Adding sortedIterables.py to my github Python_Loop repo.

---
+++ sortedIterables.py
@@ -0,0 +1,22 @@
+list_numbers = [11,12,3,41,15,26]
+
+print(sorted(list_numbers))
+
+print()
+print("Display in reverse order.")
+print(sorted(list_numbers,reverse=True))
+
+
+print()
+print("Sorting Dictionary.")
+my_dictionary = {
+
+    "Z":5,
+    "X":10,
+    "A":8,
+    "D":15
+}
+
+print(sorted(my_dictionary))
+print("Sorting Dictionary values.")
+print(sorted(my_dictionary.values()))
