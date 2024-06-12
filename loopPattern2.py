703149563d3c993178b9b45b2a47cb5e4fc9ebd7
Reil Tusoy <reiltusoy331@gmail.com>
2024-06-12 15:29:27 +0800

    Adding loopPattern2.py to my github Python_Loop repo.

---
+++ loopPattern2.py
@@ -0,0 +1,20 @@
+
+for row in range(4):
+    for char in range(row+1):
+        print("#",end="")
+    print()
+
+### Target Output ###
+#
+##
+###
+####
+
+
+### other solution non nested loop ###
+
+# for char in range(5):
+#    print("#"*char,end="")
+#    print()
+
+
