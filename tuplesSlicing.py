7b436a8dba02cba2b456eaf2e75e5f3be2ef57eb
Reil Tusoy <reiltusoy331@gmail.com>
2024-06-18 15:31:59 +0800

    Adding tuplesSlicing.py to my github Python_Loop repo.

---
+++ tuplesSlicing.py
@@ -0,0 +1,11 @@
+languages =("English","Spanish","Chinese","Hindi")
+
+print(languages[1:3])
+
+print(languages[:1])
+
+#Reverse Order
+print(languages[::-1])
+
+#concantenate tuples
+print(languages+("Tagalog","Bahasa"))
