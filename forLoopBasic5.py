efdb2c9a47db6e52ef412ba741624962c9663e27
Reil Tusoy <reiltusoy331@gmail.com>
2024-06-21 15:37:24 +0800

    Adding forLoopBasic5.py to my github Python_Loop repo.

---
+++ forLoopBasic5.py
@@ -0,0 +1,17 @@
+
+numbers =[2,3,4,5,6]
+print("List before multiplied by 2")
+print(numbers)
+
+for num in range(len(numbers)):
+    numbers[num] = numbers[num] *2
+
+print()
+print("List after multiplied by 2")
+print(numbers)
+
+
+              
+    
+
+
