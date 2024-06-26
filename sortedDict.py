ce802f29dde85c59c96b332f4e83b1f1d643d7b9
Reil Tusoy <reiltusoy331@gmail.com>
2024-06-26 14:57:29 +0800

    Adding sortedDict.py to my github Python_Loop repo.

---
+++ sortedDict.py
@@ -0,0 +1,9 @@
+my_dict ={'A':3,'B':1,'C':7,'D':4}
+
+sorted_dict=sorted(my_dict)
+print(sorted_dict)
+
+print()
+
+sorted_dict_values=sorted(my_dict.values(),reverse=True)
+print(sorted_dict_values)
