f2494d7533fbc9c84bb994e9e4e5b59f81a0dc13
Reil Tusoy <reiltusoy331@gmail.com>
2024-07-11 15:34:04 +0800

    Adding DesignPattern3.py to my github Python_Loop repo.

---
+++ DesignPattern3.py
@@ -0,0 +1,19 @@
+
+row = 4
+
+for i in range(row,0,-1):
+    print("*"*(i+1-1))
+
+for j in range(row-1):
+    print("*"*(j+2))
+
+
+
+#  Target Pattern Printing
+#    ****
+#    *** 
+#    **
+#    *
+#    ** 
+#    ***
+#    ****
