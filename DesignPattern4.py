f8fde1f3af71a05504b44cc6359c6bc16f2a20be
Reil Tusoy <reiltusoy331@gmail.com>
2024-07-11 15:34:48 +0800

    Adding DesignPattern4.py to my github Python_Loop repo.

---
+++ DesignPattern4.py
@@ -0,0 +1,15 @@
+num_row =10
+
+iteration=0
+for j in range(1,11):        
+    if j % 2 == 0:        
+        iteration+=1
+        print(f"{j} "*iteration)
+
+
+# Target Pattern Printing
+# 2 
+# 4 4
+# 6 6 6
+# 8 8 8 8
+# 10 10 10 10 10
