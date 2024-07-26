f38559f77ea751cbf49e40e663eee496508732bb
Reil Tusoy <reiltusoy331@gmail.com>
2024-07-26 15:54:29 +0800

    Adding loopPattern4.py to my github Python_Loop repo.

---
+++ loopPattern4.py
@@ -0,0 +1,12 @@
+num_row = 4
+
+for i in range(num_row):
+    for j in range(num_row):
+        print("*",end=' ')
+    print()
+
+# other method
+# for i in range(1,num_row+1):    
+#    print("* "*num_row,end='')
+#    print()
+
